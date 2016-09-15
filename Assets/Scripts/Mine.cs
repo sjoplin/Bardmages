bb018dbad217a4b744184a4732094e8ba0ae8d71
kkiniaes <kkiniaes@gmail.com>
2016-09-15 16:36:07 -0400

    Merged 'Mine Tune' pull request

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mine : Attack {
+	void Start () {
+        transform.GetChild(0).GetComponent<ParticleSystem>().Stop();
+
+        //TODO: Color should depend on the color of the mine's owner
+
+        base.Start();
+
+        if (this.crit)
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+            GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.magenta);
+            transform.GetChild(0).GetComponent<ParticleSystem>().startColor = Color.magenta;
+        }
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        //Player won't detonate the mine
+        if (other.gameObject.GetComponent<PlayerControl>() 
+            && other.gameObject.GetComponent<PlayerControl>().player != agressor)
+        {
+            base.OnTriggerEnter(other);
+
+            transform.GetChild(0).GetComponent<ParticleSystem>().Play();
+            Destroy(this.gameObject, .1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Tune_AttackBackwards.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tune_AttackBackwards : Tune
+{
+    public override void TuneComplete(bool crit)
+    {
+        base.TuneComplete(crit);
+
+        GameObject temp = (GameObject)GameObject.Instantiate(spawnObject, ownerTransform.position + -ownerTransform.forward * 2f, ownerTransform.rotation);
+        temp.GetComponent<Attack>().agressor = ownerTransform.GetComponent<PlayerControl>().player;
+        temp.GetComponent<Spawnable>().Crit(crit);
+    }
+}
