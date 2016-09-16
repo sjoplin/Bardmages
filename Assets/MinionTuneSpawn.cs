8f7a1a474effcd4f39098f4d988765355179e6ca
AlonFisch <alfischer@att.net>
2016-09-16 02:07:53 -0400

    Update 9/16/16
    Minion Tune Start

---
+++ Assets/MinionTuneSpawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionTuneSpawn : MonoBehaviour, Spawnable {
+
+	public PlayerID owner;
+	private bool crit;
+
+	public void Crit (bool crit)
+	{
+		this.crit = crit;
+	}
+	public void Owner (PlayerID owner)
+	{
+		this.owner = owner;
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (crit) {
+			Destroy (this.gameObject, 10f);
+		} else {
+			Destroy (this.gameObject, 5f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
