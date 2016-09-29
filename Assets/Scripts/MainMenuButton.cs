bcda37f7cd6eeb1861186e9a79f27d290a2977b9
kkiniaes <kkiniaes@gmail.com>
2016-09-29 18:59:42 -0400

    Main menu and art assets
    - added piano model from Violet
    - updated player materials
    - updated main menu

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButton : PhysicalButton {
+
+	public float raisedHeight = 5f;
+
+	private float initialHeight;
+
+	void Start() {
+		base.Start();
+		initialHeight = transform.position.y;
+	}
+
+	protected override void HandleHover ()
+	{
+		if(transform.position.y < raisedHeight) {
+			transform.Translate(Vector3.up*Time.deltaTime*15f);
+		} else {
+			transform.position = new Vector3(transform.position.x,raisedHeight, transform.position.z);
+		}
+		base.HandleHover ();
+	}
+
+	protected override void HandleNormal ()
+	{
+		if(transform.position.y > initialHeight) {
+			transform.Translate(-Vector3.up*Time.deltaTime*15f);
+		} else {
+			transform.position = new Vector3(transform.position.x,initialHeight, transform.position.z);
+		}
+		base.HandleNormal ();
+	}
+
+	protected override void HandlePressed ()
+	{
+		base.HandlePressed ();
+		StopCoroutine(ButtonPressed());
+		StartCoroutine(ButtonPressed());
+	}
+
+	private IEnumerator ButtonPressed() {
+		float timer = 0f;
+		Vector3 startingScale = transform.localScale;
+		transform.localScale *= 1.5f;
+
+		while (timer < 1f) {
+			timer += Time.deltaTime;
+			transform.localScale = Vector3.Lerp(transform.localScale, startingScale, timer);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
