0758b77dc0701890d2072ab2abfce935c9b43f97
Jonathan Hunter <hunterjonathant@gmail.com>
2016-10-14 10:55:03 -0400

    Level Loading

---
+++ Assets/Scripts/Data/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Data
+{
+    class LevelLoader : MonoBehaviour
+    {
+        /// <summary> The minimum amount of time in seconds to wait before loading the next scene. </summary>
+        [SerializeField]
+        [Tooltip("The minimum amount of time in seconds to wait before loading the next scene.")]
+        private float waitTime;
+
+        /// <summary> Reference to the data class with all of the properties needed. </summary>
+        private Data data;
+        /// <summary> Reference to the load operation to track it. </summary>
+        private AsyncOperation op;
+        /// <summary> Simple timer. </summary>
+        private float time;
+
+        void Start()
+        {
+            data = FindObjectOfType<Data>();
+            op = SceneManager.LoadSceneAsync(data.level);
+            op.allowSceneActivation = false;
+            time = waitTime;
+        }
+
+        void Update()
+        {
+            if ((time -= Time.deltaTime) < 0)
+                op.allowSceneActivation = true;
+        }
+    }
+}
