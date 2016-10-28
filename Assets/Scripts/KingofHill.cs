e7b89aebbbc3c4d8df2742006657410db024bf45
AlonFisch <alfischer@att.net>
2016-10-28 17:28:03 -0400

    update
    check previous commit for details

--- Assets/Scripts/KingofHill.cs
+++ Assets/Scripts/KingofHill.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class KingofHill : MonoBehaviour {
 
 	public BaseControl king = null;
+	private float nextUpdate = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -12,19 +13,31 @@ public class KingofHill : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (king != null && !king.GetComponent<PlayerLife>().Alive) {
-			transform.parent = null;
-			this.GetComponentInChildren<MeshRenderer> ().enabled = true;
-			transform.position = king.GetComponent<PlayerLife>().PositionOfDeath;
-			if (king.GetComponent<PlayerLife> ().FellOffMap) {
-				transform.position = Vector3.up;
-				king.GetComponent<PlayerLife> ().FellOffMap = false;
+		if (king != null) {
+			if (!king.GetComponent<PlayerLife> ().Alive) {
+				transform.parent = null;
+				this.GetComponentInChildren<MeshRenderer> ().enabled = true;
+				transform.position = king.GetComponent<PlayerLife> ().PositionOfDeath;
+				if (king.GetComponent<PlayerLife> ().FellOffMap) {
+					transform.position = Vector3.up;
+					king.GetComponent<PlayerLife> ().FellOffMap = false;
+				}
+				king = null;
+				this.GetComponentInChildren<ParticleSystem> ().enableEmission = false;
+				this.GetComponent<Rigidbody> ().velocity.Set (0, 0, 0);
+				this.GetComponent<Rigidbody> ().isKinematic = false;
+			} else {
+				if (Time.time >= nextUpdate) {
+					nextUpdate = Time.time + 1;
+					UpdateEverySecond (king);
+				}
 			}
-			king = null;
-			this.GetComponentInChildren<ParticleSystem>().enableEmission = false;
-			this.GetComponent<Rigidbody> ().velocity.Set(0,0,0);
-			this.GetComponent<Rigidbody> ().isKinematic = false;
-		} 
+		}
+	}
+
+	void UpdateEverySecond(BaseControl k) {
+		Debug.Log ("hello");
+		Assets.Scripts.Data.RoundHandler.Instance.AddScore (k.player);
 	}
 
 	void OnControllerColliderHit(ControllerColliderHit hit) {
