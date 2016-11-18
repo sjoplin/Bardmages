2ca8d084a65d6a41d2af4fd6a9cff7fcd1b8ece5
sjoplin <sjoplin@lawn-128-61-61-33.lawn.gatech.edu>
2016-11-18 02:25:34 -0500

    All coliseum files and hopefully fixed menu description text

---
+++ Assets/Scripts/ColiseumSpectacter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColiseumSpectacter : MonoBehaviour {
+	// Use this for initialization
+	Random rand;
+	public GameObject toBeThrown;
+	public GameObject spawnLoc;
+	private float randomNumLim = 1000f;
+	void Start () {
+		rand = new Random();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int fire = 0;
+		if (randomNumLim > 1) {
+			randomNumLim -= Time.deltaTime;
+			fire = (int)(Random.value * randomNumLim);
+		} else {
+			fire = 1;
+		}
+		if (fire == 1) {
+			GameObject temp = (GameObject) Object.Instantiate (toBeThrown, spawnLoc.transform);
+			temp.transform.position = spawnLoc.transform.position;
+			temp.transform.rotation = spawnLoc.transform.rotation;
+		}
+	
+	}
+}
